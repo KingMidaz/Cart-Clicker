c6864257077c5ac89ca3d50061a5c02cbce9cbb6
David Scragg <dscragg@live.com>
2018-06-12 18:44:08 +0200

    Mostly functional; Jordan dropdown still not selecting

---
+++ clicker.js
@@ -0,0 +1,97 @@
+var requiredSizes = [9, 9.5, 10, 8.5, 8, 10.5];
+console.log("test");
+
+console.log("Running...");
+
+var  x = setInterval(function() {
+	var all = document.getElementsByTagName("*");
+	var sizes = [];
+	var cart = [];
+
+	for (var i = 0, max = all.length; i < max; i++) {
+		for(var t = 0; t < requiredSizes.length; t++) {
+			// Find all buttons relating to size options
+			if (all[i].textContent.includes(requiredSizes[t]) && looksLikeSize(all[i].textContent, requiredSizes[t])) { 
+							sizes.push(all[i]);
+			}
+		}
+		// Find add to cart button
+		if (!(all[i].outerHTML.includes("script") ||
+					all[i].outerHTML.includes("style")) && looksLikeAddToCart(all[i].textContent)) { 
+						if(!all[i].outerHTML.includes("button")) {
+							cart.push(all[i].parentElement);
+						} else {
+							cart.push(all[i]);
+						}
+					}
+	}
+
+	console.log(sizes);
+	console.log(cart);
+
+	for (let p = 0; p < sizes.length; p++) {
+		$(sizes[p]).click();
+		for(let o = 0; o < cart.length; o ++) {
+			$(cart[o]).click();
+		}
+	}
+		console.log("...Finished");
+}, 5000);
+
+function looksLikeSize(text, size) {
+	let words = [];
+	let v = 0;
+	let tmp = ""
+	
+	for(let x = 0; x < text.length; x++)	{
+		if(tmp == "" && text[x] != " ") { 
+			tmp += text[x];
+		} else if (tmp != "" && text[x] == " ") {
+			words[v] = tmp;
+			v++;
+			tmp = ""
+		} else if (text[x] != " ") {
+			tmp += text[x];
+		}
+	}
+	words[v] = tmp;
+	
+	if(words.length > 10) { return false; }
+
+	for (let m = 0; m < words.length; m++) { 
+		if(words[m] == size) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function looksLikeAddToCart(text) {
+	let words = [];
+	let v = 0;
+	let tmp = ""
+	
+	let letters = /^[A-Za-z]+$/;
+	let requiredWords = ["add", "to", "cart", "bag"];
+
+	for(let x = 0; x < text.length; x++)	{
+		if(tmp == "" && text[x].match(letters)) { 
+			tmp += text[x];
+		} else if (tmp != "" && !text[x].match(letters)) {
+			words[v] = tmp;
+			v++;
+			tmp = ""
+		} else if (text[x].match(letters)) {
+			tmp += text[x];
+		}
+	}
+	words[v] = tmp;
+
+	for (let m = 0; m < words.length; m++) { 
+		if(words[m].toLowerCase() == requiredWords[0] && words[m+1].toLowerCase() == requiredWords[1] 
+				&& (words[m+2].toLowerCase() == requiredWords[2] || words[m+2].toLowerCase() == requiredWords[3])) {
+			return true;
+		}
+	}
+	return false;
+}
