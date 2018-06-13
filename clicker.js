63d29288fed9927956b5d42b26c904d7550e1be9
David Scragg <dscragg@live.com>
2018-06-14 00:59:58 +0200

    Fixed ordering issue

--- clicker.js
+++ clicker.js
@@ -5,60 +5,33 @@ function runForever() {
 	alert("Running Forever...");
 
 	x = setInterval(function () {
-		var all = document.getElementsByTagName("*");
-		var sizes = [];
-		var cart = [];
-		for (var i = 0, max = all.length; i < max; i++) {
-			for (var t = 0; t < requiredSizes.length; t++) {
-				// Find all buttons relating to size options
-				if (all[i].textContent.includes(requiredSizes[t]) && looksLikeSize(all[i].textContent, requiredSizes[t])) {
-					sizes.push(all[i]);
-				}
-			}
-			// Find add to cart button
-			if (!(all[i].outerHTML.includes("script") ||
-				all[i].outerHTML.includes("style")) && looksLikeAddToCart(all[i].textContent)) {
-				if (!all[i].outerHTML.includes("button")) {
-					cart.push(all[i].parentElement);
-				}
-				else {
-					cart.push(all[i]);
-				}
-			}
-		}
-		//console.log(sizes);
-		//console.log(cart);
-		for (let p = 0; p < sizes.length; p++) {
-			$(sizes[p]).click();
-			sizes[p].dispatchEvent(mouseUp);
-			for (let o = 0; o < cart.length; o++) {
-				$(cart[o]).click();
-			}
-		}
-		
-		var notifications = document.getElementsByClassName("notification-button");
-
-		for (let g = 0; g < notifications.length; g++) {
-			$(notifications[g]).click();
-		}
-
-		console.log("...Finished");
+		executeSelection();
 	}, 500);
 }
+
+
 
 function runOnce() {
 	alert("Running Once...");
 	
+	executeSelection();
+}
+
+function executeSelection() {
 	var all = document.getElementsByTagName("*");
 	var sizes = [];
 	var cart = [];
-	for (var i = 0, max = all.length; i < max; i++) {
-		for (var t = 0; t < requiredSizes.length; t++) {
+
+	for (var t = 0; t < requiredSizes.length; t++) {
+		for (let i = 0, max = all.length; i < max; i++) {
 			// Find all buttons relating to size options
 			if (all[i].textContent.includes(requiredSizes[t]) && looksLikeSize(all[i].textContent, requiredSizes[t])) {
 				sizes.push(all[i]);
 			}
 		}
+	}
+
+	for (let i = 0, max = all.length; i < max; i++) {
 		// Find add to cart button
 		if (!(all[i].outerHTML.includes("script") ||
 			all[i].outerHTML.includes("style")) && looksLikeAddToCart(all[i].textContent)) {
@@ -70,21 +43,28 @@ function runOnce() {
 			}
 		}
 	}
+	
 	//console.log(sizes);
 	//console.log(cart);
+
 	for (let p = 0; p < sizes.length; p++) {
 		$(sizes[p]).click();
 		sizes[p].dispatchEvent(mouseUp);
-		console.log(sizes[p]);
 		for (let o = 0; o < cart.length; o++) {
 			$(cart[o]).click();
 		}
 	}
+
+	var notifications = document.getElementsByClassName("notification-button");
+	for (let g = 0; g < notifications.length; g++) {
+		$(notifications[g]).click();
+	}
+
 	console.log("...Finished");
 }
 
 function runTest() {
-	alert("Running Once...");
+	alert("Running Test...");
 	
 	var all = document.getElementsByTagName("*");
 	var sizes = [];
